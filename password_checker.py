0938aa1f90a5f00ed974c734e59e93c5d64c893c
drmtcpt <nadjafliali@gmail.com>
2025-03-10 16:31:21 +0400

    Second change

--- password_checker.py
+++ password_checker.py
@@ -1,14 +1,12 @@
 def check_password(password):
-    has_upr = any(c.isupper() for c in password)  # Исправлено 'chdr' на 'c'
-    has_lwr = any(c.islower() for c in password)  # Исправлено 'char' на 'c'
-    has_num = any(c.isdigit() for c in password)  # Исправлено 'vhar' на 'c'
-    is_long_enough = len(password) >= 8  # Исправлено 'isLongEnough' на 'is_long_enough'
-    
+    has_upr = any(c.isupper() for c in password)  
+    has_lwr = any(c.islower() for c in password)  
+    has_num = any(c.isdigit() for c in password)  
+    is_long_enough = len(password) >= 8      
     if all([has_upr, has_lwr, has_num, is_long_enough]):
         print('Strong password')
     else:
         print('Weak password')
 
-# Запрос пароля у пользователя и вызов функции проверки
 password = input('Enter a password: ')
 check_password(password)
